7f6adaceb7e4fb48fd5e9a2cc34e262e4e51d621
sj <sj@gmail.com>
2016-08-28 11:00:10 -0700

    Spring Cloud Config Client

---
+++ src/main/java/com/example/controllers/Greeter.java
@@ -0,0 +1,28 @@
+package com.example.controllers;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@Component
+@RestController
+public class Greeter {
+	
+	@Value("${message.greeting}")
+	String greeting;
+	@Value("${server.port}")
+	int port;
+	@Value("${configuration.projectName}")
+	String projectName;
+	
+	@RequestMapping(value="/",produces="application/json")
+	public List<String> index(){
+		List<String> env=Arrays.asList("message.greeting is "+greeting+" server.port is "+port+" configuration.projectName is "+projectName );
+		return env;
+	}
+}
